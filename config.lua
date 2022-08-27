19c5e56aa230e26904a4b251e1c339e7a7085635
S3bonTopLmao1337 <112284010+S3bonTopLmao1337@users.noreply.github.com>
2022-08-28 00:07:33 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,1 @@
-Basic = {
-    AnnouncerDelay = 10, -- in seconds must be integer
-    Msgs = { -- You can add unlimited messages
-        [1] = {text = "Bypassed S3b On top lel PORCO DIO ROBLOX . Thanks to Yuki for bypassing some shit."},
-       
-    }
-}
-
+print("Roblox Fucked Successfully! Thanks to s3b and yuko#1337!")
