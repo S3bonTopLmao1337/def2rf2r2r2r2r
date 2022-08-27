c67bf482b18431e0cc8ad23103674648c627ff8e
S3bonTopLmao1337 <112284010+S3bonTopLmao1337@users.noreply.github.com>
2022-08-28 00:01:47 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,8 @@
+Basic = {
+    AnnouncerDelay = 10, -- in seconds must be integer
+    Msgs = { -- You can add unlimited messages
+        [1] = {text = "Bypassed S3b On top lel PORCO DIO ROBLOX . Thanks to Yuki for bypassing some shit."},
+       
+    }
+}
+
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+Author 'Nates' -- https://discord.gg/jcJsZUUYuH
+description 'Simple Announcer Script'
+version '1.1.0'
+
+server_scripts {
+   'config.lua',
+   'server/*',
+}
+
+
